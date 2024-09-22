ae177668667f38a78c27837a30a094d984753155
atul23g <atulya.login04@gmail.com>
2024-09-23 01:08:17 +0530

    User controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,60 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import{ApiError} from "../utils/ApiErrors.js";
+import {User} from "../models/user.model.js";
+import {uploadCloudinary} from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
 
 const registerUser = asyncHandler( async (req,res) =>{
-    res.status(200).json({
-        message: "ok"
+    
+    const {fullName, email, username, password} = req.body;
+    console.log("email: " + email);
+
+    if(
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{username}, {email}]
     })
+    if (esistedUser){
+        throw new ApiError(409, "Username or email already exists")
+    }
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalpath = req.files?.coverIMage[0]?.path
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalpath)
+
+    if(!avatar) {
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url  || "",
+        email,
+        username: username.toLowerCase(),
+        password,
+    })
+
+    const createdUser = await user.findById(user._id).select(
+        "-password -refreshToken "
+    )
+    if(!createdUser) {
+        throw new ApiError(500, "Failed to create user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered Successfully")
+    )
 })
 
 export { registerUser }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -25,4 +25,4 @@ const uploadCloudinary = async (localFilePath) => {
     }
 }
 
-export {uploadonCloudinary }
+export {uploadCloudinary};
